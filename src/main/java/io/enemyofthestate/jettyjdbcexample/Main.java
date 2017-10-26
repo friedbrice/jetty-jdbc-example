2877bcd2491a94ad66aa1f0635e899fd466fd4f1
Daniel Brice <dbrice@cj.com>
2017-10-26 03:20:18 -0700

    done for the night

--- src/main/java/io/enemyofthestate/jettyjdbcexample/Main.java
+++ src/main/java/io/enemyofthestate/jettyjdbcexample/Main.java
@@ -5,7 +5,7 @@ import org.httpobjects.Request;
 import org.httpobjects.Response;
 import org.httpobjects.jetty.HttpObjectsJettyHandler;
 
-import static org.httpobjects.util.Method.GET;
+import java.sql.*;
 
 public class Main {
 
@@ -15,15 +15,34 @@ public class Main {
 
         HttpObject rootResource = new HttpObject("/") {
 
-            public Response get(Request req){
+            public Response get(Request req) {
                 return OK(Text("Hello, World!"));
             }
         };
 
         HttpObject starWarsResource = new HttpObject("/starwars") {
 
-            public Response get(Request req){
-                throw new RuntimeException("Not yet implemented.");
+            public Response get(Request req) {
+
+                try {
+                    Connection conn = DriverManager.getConnection("jdbc:sqlite:sample.db");
+                    PreparedStatement prep = conn.prepareStatement("SELECT * FROM characters");
+                    ResultSet rs = prep.executeQuery();
+                    StringBuilder sb = new StringBuilder();
+                    while (rs.next()) {
+                        int id = rs.getInt("id");
+                        String name = rs.getString("name");
+                        sb.append("id: ");
+                        sb.append(id);
+                        sb.append(", name: ");
+                        sb.append(name);
+                        sb.append("\n");
+                    }
+                    String results = sb.toString();
+                    return OK(Text(results));
+                } catch(Throwable err) {
+                    throw new RuntimeException(err);
+                }
             }
         };
 
